a8a3e46b4da865dc6c17a013a573ef9c7d2b11db
root <burhan_abdi@hotmail.co.uk>
2015-11-11 23:24:39 +0000

    updated index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php $link = mysql_connect('localhost', 'root'); ?>
-<?php $query = sprintf("SELECT table_schema "Data Base Name", sum( data_length + index_length ) / 1024 / 1024 "Database size in MB", sum( data_free )/1024 / 104 "Free Space in MB" FROM information_schema.TABLES GROUP BY table_schema;"); ?>
+
 
 <html>
 <head>
@@ -20,19 +20,13 @@
 <body>
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello world!"; ?></h1>
+	<p><a href="v3ckt0r.github.io/index2.html">Click me"</a>if your brave. Or play a pointless <a href="v3ckt0r.github.io/canvas_cubes.html">game here</a>. hint - click the squares!</p>
 	<?php if(!$link) { ?>
 		<h2>Can't connect to local MySQL Server!</h2>
 	<?php } else { ?>
 		<h2>MySQL Server version: <?php echo mysql_get_server_info(); ?></h2>
 	<?php } ?>
-	<?php
-		if (!$query) {
-			die("Something went wrong ;-)");
-		}
 
-		//result
-		$result = mysql_query($query);
-	?>
-		<h2>Here is the disk space of mysql databases /br <?php print $result; ?></h2>
+
 </body>
 </html>
